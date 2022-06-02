a2c608b07595ed4fa9f8c3f7cd120b4d999e4c6d
Malek <johnmalek54@gmail.com>
2022-06-02 06:33:34 -0700

    c programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -7,8 +8,9 @@
  */
 
 int main(void)
-{
-	fprintf(stderr, "and that piece of art is useful\"  - Dora Korpar, 2015-10-19\n");
+}
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);	
 }
 
