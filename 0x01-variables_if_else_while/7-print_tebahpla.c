7e9ba3185766c64b537ca65d293c892380ba9e2a
Malek <johnmalek54@gmail.com>
2022-06-03 07:20:02 -0700

    Print the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'z'; alph <= 'a'; alph++)
+		printf("%d", alph);
+	printf("\n");
+	return (0);
+}
