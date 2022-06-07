44ea2c31f15bf0f7a87bf861020bdfa93c611f23
Malek <johnmalek54@gmail.com>
2022-06-07 11:28:54 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+/* Print alphabet
+ * in lowercase
+ * return 0
+ * */
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+		_putchar(alph);
+	_putchar('\n');
+	return 0;
+}
