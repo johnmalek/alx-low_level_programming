fd82d10ce8e4b9508b4d85843eabdfd72491a7f8
Malek <johnmalek54@gmail.com>
2022-06-13 04:32:35 -0700

    A function that prints every character of a string followed by a new line

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,14 +6,15 @@
  */
 void puts2(char *str)
 {
-	int i = 0, l = 0;
+	int len = 0, i = 0;
 
-	while (str[l] != '\0')
-		i++;
-	
-	l -= 1;
+	while (str[len] != '\0')
+		len++;
 
-	for (; i <= l; i += 2)
+	len -= 1;
+
+	for (; i <= len; i += 2)
 		_putchar(str[i]);
+
 	_putchar('\n');
-}
+
