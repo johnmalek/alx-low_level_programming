018325c62ec39158fecf2c97debfe5f227cee72f
Malek <johnmalek54@gmail.com>
2022-06-13 03:29:23 -0700

    A function that prints a string to the stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _puts - a function that prints a string to the stdout
+ * @str: input string pointer
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
+
