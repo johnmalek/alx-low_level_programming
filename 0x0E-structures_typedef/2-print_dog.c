5f88f7f1d959908b33129e62d3143dd50f913627
Malek <johnmalek54@gmail.com>
2022-07-04 06:11:38 -0700

    a function that prints a dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,13 +17,3 @@ void print_dog(struct dog *d)
 
 	printf("Name: %s\nAge: %0.6f\nOwner: %s\n", d->name, d->age, d->owner);
 }
-int main(void)
-{
-    struct dog my_dog;
-
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
-    print_dog(&my_dog);
-    return (0);
-}
