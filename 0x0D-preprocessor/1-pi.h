46532bef4ac45d2af81b4077b8418ebe57a9805d
Malek <johnmalek54@gmail.com>
2022-07-04 04:35:56 -0700

    PI macro

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,14 +1,4 @@
-#include <stdio.h>
 #ifndef PI
 #define PI 3.14159265359
 #endif
-int main(void)
-{
-    float a;
-    float r;
 
-    r = 98;
-    a = PI * r * r;
-    printf("%.3f\n", a);
-    return (0);
-}
